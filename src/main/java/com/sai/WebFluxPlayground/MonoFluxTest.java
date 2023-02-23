53a727a957f60efea1d7fea3bdb6ecadee8ebe36
SaiAshish <saiashish3760@gmail.com>
2023-02-24 00:06:55 +0530

    Add files via upload

--- src/main/java/com/sai/WebFluxPlayground/MonoFluxTest.java
+++ src/main/java/com/sai/WebFluxPlayground/MonoFluxTest.java
@@ -1,4 +1,52 @@
 package com.sai.WebFluxPlayground;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class MonoFluxTest {
+
+   @Test
+   public void testMono(){
+      Mono<String> monoString = Mono.just("sai").log();
+      monoString.subscribe(System.out::println);
+   }
+
+   @Test
+   public void testMono1(){
+      Mono<?> monoString = Mono.just("sai")
+              .then(Mono.error(new RuntimeException("Exception")))
+              .log();
+      monoString.subscribe(System.out::println);
+   }
+
+   @Test
+   public void testMono2(){
+      Mono<?> monoString = Mono.just("sai")
+              .then(Mono.error(new RuntimeException("Exception")))
+              .log();
+      monoString.subscribe(System.out::println, e -> System.out.println(e.getMessage()));
+   }
+
+   @Test
+   public void testFlux(){
+      Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Hibernate", "Test")
+              .concatWithValues("Flux")
+              .log();
+//      publisher
+//      5 onNext calls are triggered
+//      consumer via subscribe()
+      stringFlux.subscribe(System.out::println);
+   }
+
+   @Test
+   public void reactiveFlowTest(){
+      Flux<?> stringFlux = Flux.just("Spring", "Spring Boot", "Hibernate", "Test")
+              .concatWithValues("Flux")
+              .concatWith(Mono.error(new RuntimeException("Exception")))
+              .concatWithValues("Flow")
+              .log();
+      stringFlux.subscribe(System.out::println);
+   }
+
 }
